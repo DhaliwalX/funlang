f6c253674c3e98e5160ebceb5566ec9782574ae9
Pushpinder Singh <dhaliwalprince@hotmail.com>
2018-12-14 10:29:22 -0800

    remove old code

--- ast/visitor.go
+++ ast/visitor.go
@@ -2,34 +2,6 @@ package ast
 
 import "fmt"
 
-type _Visitor interface {
-	VisitNilLiteral(*NilLiteral)
-	VisitNumericLiteral(*NumericLiteral)
-	VisitStringLiteral(*StringLiteral)
-	VisitBooleanLiteral(*BooleanLiteral)
-	VisitIdentifier(*Identifier)
-	VisitArgumentList(*ArgumentList)
-	VisitMemberExpression(*MemberExpression)
-	VisitPrefixExpression(*PrefixExpression)
-	VisitPostfixExpression(expression *PostfixExpression)
-	VisitBinaryExpression(*BinaryExpression)
-	VisitAssignExpression(*AssignExpression)
-	VisitArrayType(*ArrayType)
-	VisitField(*Field)
-	VisitStructType(*StructType)
-	VisitFuncType(*FuncType)
-	VisitDeclaration(*Declaration)
-	VisitDeclarationList(*DeclarationList)
-	VisitTypeDeclaration(*TypeDeclaration)
-	VisitBlockStatement(*BlockStatement)
-	VisitForStatement(*ForStatement)
-	VisitExpressionStmt(*ExpressionStmt)
-	VisitFunctionProtoType(*FunctionProtoType)
-	VisitFunctionStatement(*FunctionStatement)
-	VisitIfElseStatement(statement *IfElseStatement)
-	VisitReturnStatement(statement *ReturnStatement)
-}
-
 type Visitor interface {
 	Visit(node Node) Visitor
 }
